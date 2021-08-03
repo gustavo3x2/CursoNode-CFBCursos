a4b9af2a6f62d6868ed667a7195b7e6101abec3e
Capexoto <gustavo3x2@gmail.com>
2021-08-03 14:58:08 -0300

    Aula16

--- aula13/index.js
+++ aula13/index.js
@@ -4,18 +4,37 @@ const url = "mongodb+srv://capexoto:96962606@cluster0.dbnts.mongodb.net/myFirstD
 mongodb.connect(url,(erro,banco)=>{
   if(erro)throw erro
   const dbo =  banco.db("cfbcursos")
-  const obj = {curso:"Curso de Javascript",canal:"CFB Cursos"}
+  const obj = {curso:"Curso de HTML",canal:"CFB Cursos"}
   const colecao = "cursos"
 
+  //inserir no banco
   /*dbo.collection(colecao).insertOne(obj,(erro,resultado)=>{
     if(erro)throw erro
     console.log("1 novo cuso inserido")
+    //banco.close()
+  })*/
+
+  //Encontrar conteudo
+ /* dbo.collection(colecao).find({canal: 'CFB Cursos'},{projection:{_id:0}}).toArray((erro,resultado)=>{
+    if(erro)throw erro
+    console.log(resultado[0].curso)
     banco.close()
   })*/
 
-  dbo.collection(colecao).find({canal: 'CFB Cursos'},{projection:{_id:0}}).toArray((erro,resultado)=>{
+  //Encontrar conteudo usando Query
+  /*const query = {}
+  dbo.collection(colecao).find(query,{projection:{_id:0}}).toArray((erro,resultado)=>{
     if(erro)throw erro
-    console.log(resultado[0])
+    console.log(resultado)
+    banco.close()
+  })*/
+
+  //Sort ordena em ordem alfabetica 
+  const ordenacao = {curso:1}
+  const query = {}
+  dbo.collection(colecao).find(query).sort(ordenacao).toArray((erro,resultado)=>{
+    if(erro)throw erro
+    console.log(resultado)
     banco.close()
   })
 })
