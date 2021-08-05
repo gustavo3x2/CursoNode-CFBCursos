eb8537e111dc73b39040e76a962eb0ec676a73f6
Capexoto <gustavo3x2@gmail.com>
2021-08-05 15:11:57 -0300

    Aula 18

--- aula13/index.js
+++ aula13/index.js
@@ -4,13 +4,24 @@ const url = "mongodb+srv://capexoto:96962606@cluster0.dbnts.mongodb.net/myFirstD
 mongodb.connect(url,(erro,banco)=>{
   if(erro)throw erro
   const dbo =  banco.db("cfbcursos")
-  const obj = {curso:"Curso de HTML",canal:"CFB Cursos"}
+  //const obj = {curso:"Curso de HTML",canal:"CFB Cursos"}
   const colecao = "cursos"
 
   //inserir no banco
   /*dbo.collection(colecao).insertOne(obj,(erro,resultado)=>{
     if(erro)throw erro
     console.log("1 novo cuso inserido")
+    //banco.close()
+  })*/
+
+  /*const obj = [
+    {curso:"Curso de Javascript",canal:"CFB Cursos"},
+    {curso:"Curso de Javascript",canal:"CFB Cursos"},
+    {curso:"Curso de Javascript",canal:"CFB Cursos"}
+  ]
+  dbo.collection(colecao).insertMany(obj,async(erro,resultado)=>{
+    if(erro)throw erro
+    await console.log(resultado.insertedCount +" novo(s) cuso(s) inserido(s)")
     //banco.close()
   })*/
 
@@ -45,11 +56,13 @@ mongodb.connect(url,(erro,banco)=>{
     if(erro)throw erro
     console.log('1 curso deletado')
   })*/
+
+  // Delettar varios objetos da coleção
   let query = {curso:/.t/}
 
-  dbo.collection(colecao).deleteMany(query, (erro,resultado)=>{
+  dbo.collection(colecao).deleteMany(query,async (erro,resultado)=>{
     if(erro)throw erro
-    console.log('cursos deletados')
+    await console.log(resultado.deletedCount + ' curso(s) deletado(s)')
   })
 
   query = {}
